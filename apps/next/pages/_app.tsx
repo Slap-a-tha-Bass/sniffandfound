da834e190c334a11e43537dd2467409f6f7ec0fb
Corey DeLoach <cdeloach16@gmail.com>
2022-03-26 16:09:40 -0500

    initial commit

---
+++ apps/next/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { Provider } from 'app/provider'
+import Head from 'next/head'
+import React from 'react'
+import type { SolitoAppProps } from 'solito'
+import 'raf/polyfill'
+
+function MyApp({ Component, pageProps }: SolitoAppProps) {
+  return (
+    <>
+      <Head>
+        <title>Sniff & Found</title>
+        <meta
+          name="description"
+          content="A social network for pets and finding lost pets."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Provider>
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  )
+}
+
+export default MyApp
